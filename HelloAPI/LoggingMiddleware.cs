32ae03ca73e19bc91bfdc5379583f0d07776da0a
kamalelhaddad <kamalel7addad@gmail.com>
2024-01-02 18:50:35 +0200

    adding web API app to the solution

---
+++ HelloAPI/LoggingMiddleware.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http.Extensions;
+namespace HelloAPI;
+
+public class LoggingMiddleware(RequestDelegate next, ILogger<LoggingMiddleware> logger) {
+    public async Task InvokeAsync(HttpContext context) {
+        logger.LogInformation("Request URL: {URL}", context.Request.GetDisplayUrl());
+        await next(context);
+    }
+}
